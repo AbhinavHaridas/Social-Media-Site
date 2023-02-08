f5bf6139a903d242e05954f16959e41f5ce52cfe
Abhinav <102739327+AbhinavHaridas@users.noreply.github.com>
2023-02-08 20:55:42 +0530

    Revert "Revert "Backend added""
    This reverts commit f405a36a7153ddd0965524280da1d1771123fe79.

---
+++ src/mikro-orm.config.ts
@@ -0,0 +1,18 @@
+import { Configuration, Options } from "@mikro-orm/core";
+import { defineConfig } from "@mikro-orm/postgresql";
+import { PostgreSqlDriver } from "@mikro-orm/postgresql";
+import path from "path";
+import { Posts } from "./Entities/post";
+
+// Config for database
+export default defineConfig({
+    migrations: {
+            path: path.join(__dirname, "./Migrations")
+        },
+        entities: [Posts],
+        allowGlobalContext: true,
+        type: "postgresql",
+        dbName: "social-media-site-db",
+        user: "abhij",
+        debug: true,
+});
